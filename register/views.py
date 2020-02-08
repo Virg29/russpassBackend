c823961905fb7b497b1f248cf0f0bd4eb9b39ad4
Virg29 <256in4@gmail.com>
2020-02-08 15:58:15 +0300

    register, login

--- register/views.py
+++ register/views.py
@@ -10,12 +10,20 @@ from rest_framework.response import Response
 class RegisterNewUser(generics.CreateAPIView):
 	serializer_class = UserRegister
 
-@api_view(['POST'])
+@api_view(http_method_names=['POST'])
 def Login(request):
 	userLogin = request.data.get("login")
 	password = request.data.get("password")
-	user = User.objects.filter(name=userLogin);
-	#password = user.get(password)
+	user = User.objects.filter(name=userLogin)
+	if(len(user)==1):
+		user = user.filter(password=password)
+		if(len(user)==1):
+			return Response({"status": True})
+		else:
+			return Response({"status": False})
+	else:
+		return Response({"status": False})
+	#passwordFromDb = user.get(password)
 	#print(password)
-	return Response({"status": True})
+	
 	#return Response(request)
